554f179f7220420888155b27d855d35a805e740c
vincentchui <vincentchuicalifornia@gmail.com>
2015-10-06 06:48:25 -0700

    code

---
+++ Histogram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Histogram
+{
+    class Histogram
+    {
+        private double[] counter = new double[10] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        public void getRange(double counter)
+        {
+
+        }
+
+        public void add(double x)
+        {
+            x = x + 1;
+        }
+
+        public void reset()
+        {
+            
+
+        }
+
+        public void plotFrequency()
+        {
+
+        }
+
+        public void plotCumlative()
+        {
+
+        }
+
+    }
+}
+/*OUTPUT
+*/
---
+++ RandomNumberTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Histogram
+{
+    class RandomNumberTester
+    {
+        static void Main(string[] args)
+        {
+            Histogram h = new Histogram();
+        }
+    }
+}
