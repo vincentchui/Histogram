30294f36899fcdc2adc511fd0a6d4e0f1bcedaf7
vincentchui <vincentchuicalifornia@gmail.com>
2015-10-07 19:08:33 -0700

    final output pasted

--- Histogram.cs
+++ Histogram.cs
@@ -13,16 +13,18 @@ namespace Histogram
         public void menu()
         {
             string choice;
-            int randomI;
-            double randomD;
-            int cumI = 0;
+            double random;
+            double cum = 0;
             Random r = new Random();
+
+            printInfo();
 
             do
             { 
                 Console.Write("\nPlot" +
                 "\nReset" +
-                "\nQuit");
+                "\nQuit" +
+                "\nEnter an choice: ");
                 choice = Console.ReadLine().ToLower();
 
                 switch (choice)
@@ -30,17 +32,19 @@ namespace Histogram
                     case "plot":
                         do
                         {
-                            Console.Write("\nFrequency or Cumulative: ");
+                            Console.Write("\nFrequency, Cumulative, or Quit: ");
                             choice = Console.ReadLine().ToLower();
 
                             if (choice == "frequency")
                             {
                                 for (int i = 0; i < 10000; i++)
                                 {
-                                    randomD = r.NextDouble();
-                                    freqCounter(randomD);
+                                    random = r.NextDouble();
+                                    freqCounter(random);
                                 }
                                 readCounter();
+
+                                reset();
                             }
                             else if (choice == "cumulative")
                             {
@@ -48,12 +52,19 @@ namespace Histogram
                                 {
                                     for (int x = 0; x < 10; x++)
                                     {
-                                        randomI = r.Next(0, 10);
-                                        cumI += randomI;
+                                        random = r.NextDouble();
+                                        cum += random;
                                     }
-                                    cumCounter(cumI);
+                                    cumCounter(cum);
+                                    cum = 0;
                                 }
                                 readCounter();
+
+                                reset();
+                            }
+                            else if(choice == "quit")
+                            {
+                                menu();
                             }
                             else
                             {
@@ -62,6 +73,7 @@ namespace Histogram
                         } while ((choice != "frequency") || (choice != "cumulative"));
                         break;
                     case "reset":
+                        Console.WriteLine("\nCounters reseted");
                         reset();
                         break;
                     case "quit":
@@ -77,7 +89,7 @@ namespace Histogram
         public void readCounter()
         {
             Console.WriteLine(
-                "\n\t\tRange\t\t\tCount" +
+                "\n\tRange\t\tCount" +
                 "\n  0 <= x < 0.1" + "\t\t" + counter[0] + 
                 "\n0.1 <= x < 0.2" + "\t\t" + counter[1] +
                 "\n0.2 <= x < 0.3" + "\t\t" + counter[2] +
@@ -90,51 +102,47 @@ namespace Histogram
                 "\n0.9 <= x < 1.0" + "\t\t" + counter[9]);
         }
             
-        public void cumCounter(int x)
+        public void cumCounter(double x)
         {
             if ((0 <= x) && (x < 1))
             {
-                counter[0]++;
+                add(0);
             }
             else if ((1 <= x) && (x < 2))
             {
-                counter[1]++;
+                add(1);
             }
             else if ((2 <= x) && (x < 3))
             {
-                counter[2]++;
+                add(2);
             }
             else if ((3 <= x) && (x < 4))
             {
-                counter[3]++;
+                add(3);
             }
             else if ((4 <= x) && (x < 5))
             {
-                counter[4]++;
+                add(4);
             }
             else if ((5 <= x) && (x < 6))
             {
-                counter[5]++;
+                add(5);
             }
             else if ((6 <= x) && (x < 7))
             {
-                counter[6]++;
+                add(6);
             }
-            if ((7 <= x) && (x < 8))
+            else if ((7 <= x) && (x < 8))
             {
-                counter[7]++;
+                add(7);
             }
-            if ((8 <= x) && (x < 9))
+            else if ((8 <= x) && (x < 9))
             {
-                counter[8]++;
+                add(8);
             }
-            if ((9 <= x) && (x < 10))
+            else 
             {
-                counter[9]++;
-            }
-            else
-            {
-                Console.WriteLine("\nNumber out of range");
+                add(9);
             }
         }
 
@@ -142,53 +150,49 @@ namespace Histogram
         {
             if((0 <= x) && (x < 0.1))
             {
-                counter[0]++;
+                add(0);
             }
             else if ((0.1 <= x) && (x < 0.2))
             {
-                counter[1]++;
+                add(1);
             }
             else if ((0.2 <= x) && (x < 0.3))
             {
-                counter[2]++;
+                add(2);
             }
             else if ((0.3 <= x) && (x < 0.4))
             {
-                counter[3]++;
+                add(3);
             }
             else if ((0.4 <= x) && (x < 0.5))
             {
-                counter[4]++;
+                add(4);
             }
             else if ((0.5 <= x) && (x < 0.6))
             {
-                counter[5]++;
+                add(5);
             }
             else if ((0.6 <= x) && (x < 0.7))
             {
-                counter[6]++;
+                add(6);
             }
-            if ((0.7 <= x) && (x < 0.8))
+            else if ((0.7 <= x) && (x < 0.8))
             {
-                counter[7]++;
+                add(7);
             }
-            if ((0.8 <= x) && (x < 0.9))
+            else if ((0.8 <= x) && (x < 0.9))
             {
-                counter[8]++;
-            }
-            if ((0.9 <= x) && (x < 1))
-            {
-                counter[9]++;
+                add(8);
             }
             else
             {
-                Console.WriteLine("\nNumber out of range");
+                add(9);
             }
         }
 
-        public void add(double x)
+        public void add(double i)
         {
-            x = x + 1;
+            counter[(int) i]++;
         }
 
         public void reset()
@@ -200,17 +204,70 @@ namespace Histogram
 
         }
 
-        public void plotFrequency()
+        public void printInfo()
         {
-
+            Console.WriteLine("\nWritten by Vincent Chui" +
+                "\nCIS 23 Fall 2015 BCC");
         }
-
-        public void plotCumlative()
-        {
-
-        }
-
     }
 }
 /*OUTPUT
+
+Written by Vincent Chui
+CIS 23 Fall 2015 BCC
+
+Plot
+Reset
+Quit
+Enter an choice: plot
+
+Frequency, Cumulative, or Quit: d
+
+INVALID INPUT
+
+Frequency, Cumulative, or Quit: frequency
+
+        Range           Count
+  0 <= x < 0.1          983
+0.1 <= x < 0.2          1004
+0.2 <= x < 0.3          1012
+0.3 <= x < 0.4          1011
+0.4 <= x < 0.5          1045
+0.5 <= x < 0.6          983
+0.6 <= x < 0.7          1006
+0.7 <= x < 0.8          1004
+0.8 <= x < 0.9          928
+0.9 <= x < 1.0          1024
+
+Frequency, Cumulative, or Quit: cumulative
+
+        Range           Count
+  0 <= x < 0.1          0
+0.1 <= x < 0.2          0
+0.2 <= x < 0.3          7
+0.3 <= x < 0.4          124
+0.4 <= x < 0.5          364
+0.5 <= x < 0.6          360
+0.6 <= x < 0.7          137
+0.7 <= x < 0.8          8
+0.8 <= x < 0.9          0
+0.9 <= x < 1.0          1024
+
+Frequency, Cumulative, or Quit: quit
+
+Plot
+Reset
+Quit
+Enter an choice: reset
+
+Counters reseted
+
+Plot
+Reset
+Quit
+Enter an choice: quit
+
+Goodbye
+
+Frequency, Cumulative, or Quit:
 */
